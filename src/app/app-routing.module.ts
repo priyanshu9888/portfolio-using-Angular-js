a508881c28acb3a38d4aea18571cc795ab8d9f7d
Priyanshu Raj <priyanshuraj9888@gmail.com>
2022-01-10 10:11:03 +0530

    s
    s

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { ServiceComponent } from './pages/service/service.component';
+
+
+
+
+
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'menu', component: MenuComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'service', component: ServiceComponent },
+]; 
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
